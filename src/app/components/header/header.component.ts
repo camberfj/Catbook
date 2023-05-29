1b457f002d36827550f0fb24bb03274b316c2d8a
Fran <franjc_17@hotmail.com>
2023-05-29 13:36:29 -0300

    Se hicieron modifiaciones en el codigo

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { UserService } from 'src/app/auth/user/user.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
+  user$ = this.userService.returnUsuario();
+
+  constructor(private userService: UserService, private router: Router) {}
+
+
+  ngOnInit(): void {}
+
+  logout(){
+    this.userService.eliminarToken();
+    this.router.navigate(['']);
+  }
 
 }
